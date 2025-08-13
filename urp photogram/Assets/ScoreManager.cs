90711934f60cf803b7195422a8b88524c0d23115
v.svistunov <v.svistunov@corp.nekki.ru>
2025-08-13 11:31:45 +0300

    рабочий вариант на 50 символов

--- urp photogram/Assets/ScoreManager.cs
+++ urp photogram/Assets/ScoreManager.cs
@@ -9,10 +9,20 @@ public class ScoreManager : MonoBehaviour
   public TextMeshProUGUI inputScore;
   public TMP_InputField inputName;
   public UnityEvent<string, int> submitScoreEvent;
+  
+
+  private const int MaxNameLength = 50;
 
   public void SubmintScore()
   {
-    submitScoreEvent.Invoke(inputName.text, int.Parse(inputScore.text));
+    string username = inputName.text;
+
+    if (username.Length > MaxNameLength)
+    {
+      username = username.Substring(0, MaxNameLength) + "...";
+      
+    }
+
+    submitScoreEvent.Invoke(username, 0);
   }
-  
 }
